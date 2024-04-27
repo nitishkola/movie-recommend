01339569ea315ab0afefbbec0f9ea96f549d1cc1
nitishkola <137293315+nitishkola@users.noreply.github.com>
2024-04-27 07:52:18 +0530

    Add files via upload

---
+++ src/Components/Datafetch.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import config from '../config'
+
+const Datafetch = () => {
+    const [posts, setPosts]=useState([])
+    const [id, setId]=useState(1)
+    const [buttonid, setbuttonid]=useState(1)
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${buttonid}`)
+          .then(res => {
+            console.log(res);
+            setPosts(res.data);
+          })
+          
+      }, [buttonid])
+      function handleClick()
+      {
+        setbuttonid(id)
+      }
+
+      
+  return (
+    <div>
+  <input type='text' value={id} onChange={e => setId(e.target.value)} />
+  <button onClick={handleClick}>FetchPost</button>
+  <p>{posts.title}</p>
+
+
+
+    {/* <ul>
+    {
+        posts.map(post=>(
+            <li key='id'>{post.title}</li>
+        ))
+    }
+    </ul> */}
+    </div>
+  )
+}
+
+export default Datafetch
---
+++ src/Components/Success.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import config from '../config'
+const success = () => {
+  return (
+    <div><center><h1>Signed In Successfully</h1></center></div>
+  )
+}
+
+export default success
